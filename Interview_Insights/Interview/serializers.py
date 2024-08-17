58cb3fb35b273750fe33956cbf451fdb93519835
kiranbaburaj <kiranbaburaj.t@gmail.com>
2024-08-17 08:50:06 +0530

    schedule Interview

--- Interview_Insights/Interview/serializers.py
+++ Interview_Insights/Interview/serializers.py
@@ -1,14 +1,17 @@
 # serializers.py
 from rest_framework import serializers
 from .models import InterviewSchedule
-
-class InterviewScheduleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = InterviewSchedule
-        fields = ['id', 'job_application', 'scheduled_time', 'duration', 'location', 'notes', 'is_confirmed']
-
 from rest_framework import serializers
 from .models import InterviewFeedback
+
+class InterviewScheduleSerializer(serializers.ModelSerializer):
+    job_seeker_name = serializers.SerializerMethodField()
+    class Meta:
+        model = InterviewSchedule
+        fields = ['id', 'job_application', 'scheduled_time', 'duration', 'location', 'notes', 'is_confirmed','job_seeker_name']
+
+    def get_job_seeker_name(self, obj):
+        return obj.job_application.job_seeker.user.full_name
 
 
 class InterviewFeedbackSerializer(serializers.ModelSerializer):
